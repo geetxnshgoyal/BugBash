5e86733020208167a4ed7dad75954e1c383ab036
Geetansh Goyal <geetanshgoyal@Geetansh-Goyal.local>
2025-10-17 18:07:22 +0530

    updated favicon

--- server.js
+++ server.js
@@ -1103,7 +1103,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/favicon.ico', (_req, res) => {
-  res.type('image/png').sendFile(path.join(__dirname, 'assets', 'favicon-48.png'));
+  res.type('image/x-icon').sendFile(path.join(__dirname, 'assets', 'favicon.ico'));
 });
 
 app.get('/robots.txt', (_req, res) => {
