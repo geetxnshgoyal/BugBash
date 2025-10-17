790650e0569170f27c3736cda5664a34f3a38f37
Geetansh Goyal <geetanshgoyal@Geetansh-Goyal.local>
2025-10-17 18:04:46 +0530

    updated favicon

--- server.js
+++ server.js
@@ -1103,7 +1103,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/favicon.ico', (_req, res) => {
-  res.type('image/png').sendFile(path.join(__dirname, 'assets', 'bugbash_logo.png'));
+  res.type('image/png').sendFile(path.join(__dirname, 'assets', 'favicon-48.png'));
 });
 
 app.get('/robots.txt', (_req, res) => {
